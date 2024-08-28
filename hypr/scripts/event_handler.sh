f97cea78b4a0a05b7695544f2affa0aaeb77406e
viplov <viplov.jain@gmail.com>
2024-08-28 16:51:04 +0100

    hyprland config update

--- hypr/scripts/event_handler.sh
+++ hypr/scripts/event_handler.sh
@@ -8,12 +8,20 @@ handle_focusedmon() {
     esac
 }
 
+handle_monitoradd() {
+    killall waybar && waybar& ;;
+    for i in $(seq 2 9);
+    do  
+        hyprctl dispatch moveworkspacetomonitor "$i DP-2"
+    done
+}
+
 handle() {
   case $1 in
     focusedmon*) handle_focusedmon "${1#focusedmon>>*}" ;;
-    monitoradded*) killall waybar && waybar& ;;
+    monitoradded*) handle_monitoradd() ;;
     monitorremoved*) killall waybar && waybar& ;;
   esac
 }
 
-socat -U - UNIX-CONNECT:/run/user/1000/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
+socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock" | while read -r line; do handle "$line"; done$line"; done
