2231b883a2423bdf9c0f64baa155685806bc6feb
Viplov Jain <viplov.jain@gmail.com>
2024-10-15 23:42:56 +0100

    fix nvim + waybar

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -28,6 +28,7 @@ return {
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
+          ['<ESC>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm { select = true },
         },
         sources = cmp.config.sources {
