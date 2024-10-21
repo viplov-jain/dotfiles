29096afbbd2d6fe317aae8bc9d9f976b0628c429
Viplov Jain <viplov.jain@gmail.com>
2024-10-21 21:21:12 +0100

    ts config

--- nvim/lua/opts/treesitter.lua
+++ nvim/lua/opts/treesitter.lua
@@ -1,5 +1,58 @@
+local ensure_installed = {
+  'query',
+  'c',
+  'cpp',
+  'haskell',
+  'nix',
+  'rust',
+  'go',
+  'zig',
+  'kotlin',
+  'odin',
+  'python',
+
+  'lua',
+  'vim',
+  'vimdoc',
+
+  'json',
+  'toml',
+  'xml',
+  'yaml',
+  'csv',
+  'markdown',
+  'markdown_inline',
+  'proto',
+  'thrift',
+  'cmake',
+  'yuck',
+
+  'dockerfile',
+  'nginx',
+  'terraform',
+
+  'sql',
+
+  'javascript',
+  'typescript',
+  'html',
+  'css',
+  'graphql',
+
+  'bash',
+  'ruby',
+  'perl',
+
+  'diff',
+  'git_config',
+  'git_rebase',
+  'gitattributes',
+  'gitcommit',
+  'gitignore',
+}
+
 require('nvim-treesitter.configs').setup {
-  ensure_installed = 'all',
+  ensure_installed = ensure_installed,
   sync_install = false,
   auto_install = false,
   highlight = {
