94095837a5f4e661a42506afddf884eb3becf7c7
Viplov Jain <viplov.jain@gmail.com>
2024-11-17 00:51:46 +0000

    fish: plugins

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,9 +1,7 @@
 if status is-interactive
     bash ~/.nix-profile/etc/profile.d/hm-session-vars.sh
     starship init fish | source
-    fzf --fish | source
-    zoxide init fish | source
-
+    zoxide init fish --cmd cd | source
     if test -e ~/init_config.fish
         source ~/init_config.fish
     end
