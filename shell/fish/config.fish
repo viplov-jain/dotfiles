1baf32a2fd84a4c2570b19cb4b721993aef4cb31
Viplov Jain <viplov.jain@gmail.com>
2024-11-28 20:30:04 +0000

    cat alias

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     starship init fish | source
     zoxide init fish --cmd cd | source
+    alias cat="bat"
     if test -e ~/init_config.fish
         source ~/init_config.fish
     end
