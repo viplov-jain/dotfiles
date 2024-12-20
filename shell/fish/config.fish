4c1da53d3b7afbe24aa279d662e32f4e29527c4f
Viplov Jain <viplov.jain@gmail.com>
2024-12-20 18:44:45 +0000

    terraform + git user fix

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -2,11 +2,14 @@ if status is-interactive
     starship init fish | source
     zoxide init fish --cmd cd | source
     alias cat="bat"
+    export MANPAGER='nvim +Man!'
     if test -e ~/init_config.fish
         source ~/init_config.fish
     end
     if test -e /nix/var/nix/profiles/default/bin
         export PATH="$PATH:/nix/var/nix/profiles/default/bin"
     end
-    export MANPAGER='nvim +Man!'
+    if test -e ~/.local/bin/env.fish 
+        source ~/.local/bin/env.fish
+    end
 end
--- systems/darwin.nix
+++ systems/darwin.nix
@@ -1,11 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   imports = [
     ./common.nix
   ];
+
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "terraform"
+    ];
+
   home.packages = with pkgs; [
-    uv
+    (terraform.withPlugins (plugin: [ plugin.aws ]))
     postgresql_16
     mongosh
     circleci-cli
