87d2464f9388a235b19ec79d1dde657bcf5ce2e4
Viplov Jain <viplov.jain@gmail.com>
2025-01-18 12:46:40 +0000

    fix eww styling

--- eww/scripts/stocks.py
+++ eww/scripts/stocks.py
@@ -1,16 +1,39 @@
-import json, requests, argparse
+import argparse
+import json
+
+import requests
+
 
 def get_request(stock):
-    return requests.get(f'https://query1.finance.yahoo.com/v8/finance/chart/{stock}', params={'metrics': 'high', 'interval': '1h', 'range': '1d'}, headers={'User-agent': 'Mozilla/5.0'})
+    return requests.get(
+        f"https://query1.finance.yahoo.com/v8/finance/chart/{stock}",
+        params={"metrics": "high", "interval": "1h", "range": "1d"},
+        headers={"User-agent": "Mozilla/5.0"},
+    )
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('stock', type=str)
+    parser.add_argument("stock", type=str)
     args = parser.parse_args()
-    data = get_request(args.stock).json()['chart']['result'][0]
-    data['did_increase'] = data['meta']['regularMarketPrice'] > data['meta']['chartPreviousClose']
-    print(json.dumps(data))
+    data = get_request(args.stock).json()["chart"]["result"][0]
+    data = data["meta"]
+    value = data["regularMarketPrice"]
+    prev_value = data["chartPreviousClose"]
+    did_increase = value > prev_value
+    print(
+        json.dumps(
+            {
+                "symbol": data["symbol"],
+                "name": data["shortName"],
+                "value": value,
+                "did_increase": did_increase,
+                "change": f"{value - prev_value:+.2f}",
+            }
+        )
+    )
     # print(data['chart']['result'][0]['meta']['regularMarketPrice'])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
