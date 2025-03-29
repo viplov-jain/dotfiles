b392570a9d3fd9d5523ab55ece819fb826645f72
Viplov Jain <viplov.jain@gmail.com>
2025-03-29 01:19:42 +0000

    rustfmt

--- nvim/lua/opts/formatter.lua
+++ nvim/lua/opts/formatter.lua
@@ -4,6 +4,7 @@ return {
   formatters_by_ft = {
     lua = { 'stylua' },
     python = { 'ruff_fix', 'ruff_organize_imports', 'ruff_format' },
+    rust = { 'rustfmt' },
 
     typescript = { 'prettier' },
     typescriptreact = { 'prettier' },
