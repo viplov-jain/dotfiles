3a67be86e3d6e121f23df8f70ee212cca3e99c1b
Viplov Jain <viplov.jain@gmail.com>
2025-06-14 13:28:15 +0100

    keymap for neotree

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+map({"n", "x"}, "<leader>t", "<cmd>Neotree toggle<cr>", {desc = "Toggle neotree"})
