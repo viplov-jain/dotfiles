5dcfd8f7e24540a6738e6f2eba7e459b4519e81f
Viplov Jain <viplov.jain@gmail.com>
2025-06-14 14:20:05 +0100

    vtsls

--- lang/typescript.nix
+++ lang/typescript.nix
@@ -3,7 +3,7 @@
   home.packages = with pkgs; [
     nodejs_24
     bun
-    vscode-langservers-extracted
+    vtsls
     nodePackages.prettier
     nodePackages.typescript-language-server
     oxlint
