984b28c8b92954bc127821bc5d18d39947a3218a
Viplov Jain <viplov.jain@gmail.com>
2026-04-25 12:49:10 +0100

    fix

--- nvim/default.nix
+++ nvim/default.nix
@@ -1,19 +1,20 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 {
   home.packages = with pkgs; [
     vim
     tree-sitter
   ];
-
   programs.neovim = {
     enable = true;
     plugins = with pkgs.vimPlugins; [
-      telescope-fzf-native-nvim # Nix handles the 'make' step during installation
+      telescope-fzf-native-nvim
       telescope-nvim
       plenary-nvim
     ];
 
     initLua = builtins.readFile ./init.lua;
+    withRuby = true;
+    withPython3 = false;
   };
   home.file = {
     ".vimrc".source = ./settings.vim;
