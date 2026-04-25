0439624797672851300dbb072d72467a4c3b6f7b
Viplov Jain <viplov.jain@gmail.com>
2026-04-25 13:15:48 +0100

    simplify plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,65 +1,42 @@
-local packages = {
-  { src = 'catppuccin/nvim', name = 'catppuccin' },
+local gh = function(x)
+  return 'https://github.com/' .. x
+end
+vim.pack.add {
+  { src = gh 'catppuccin/nvim', name = 'catppuccin' },
 
   -- Dependencies
-  'nvim-tree/nvim-web-devicons',
-  'nvim-lua/plenary.nvim',
-  'j-hui/fidget.nvim',
+  gh 'nvim-tree/nvim-web-devicons',
+  gh 'nvim-lua/plenary.nvim',
+  gh 'j-hui/fidget.nvim',
 
-  'nvim-treesitter/nvim-treesitter',
-  'neovim/nvim-lspconfig',
-  'mfussenegger/nvim-lint',
-  'stevearc/conform.nvim',
+  gh 'nvim-treesitter/nvim-treesitter',
+  gh 'neovim/nvim-lspconfig',
+  gh 'mfussenegger/nvim-lint',
+  gh 'stevearc/conform.nvim',
 
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-nvim-lsp',
-  'L3MON4D3/LuaSnip',
-  'hrsh7th/cmp-cmdline',
-  'hrsh7th/cmp-buffer',
-  'rafamadriz/friendly-snippets',
-  'windwp/nvim-autopairs',
+  gh 'hrsh7th/nvim-cmp',
+  gh 'hrsh7th/cmp-nvim-lsp',
+  gh 'L3MON4D3/LuaSnip',
+  gh 'hrsh7th/cmp-cmdline',
+  gh 'hrsh7th/cmp-buffer',
+  gh 'rafamadriz/friendly-snippets',
+  gh 'windwp/nvim-autopairs',
 
-  'akinsho/bufferline.nvim',
+  gh 'akinsho/bufferline.nvim',
 
-  'nvim-lualine/lualine.nvim',
+  gh 'nvim-lualine/lualine.nvim',
 
-  'nvim-telescope/telescope.nvim',
+  gh 'nvim-telescope/telescope.nvim',
 
-  'MunifTanjim/nui.nvim',
-  'nvim-neo-tree/neo-tree.nvim',
+  gh 'MunifTanjim/nui.nvim',
+  gh 'nvim-neo-tree/neo-tree.nvim',
 
-  'lewis6991/gitsigns.nvim',
-  'f-person/git-blame.nvim',
-  'stevearc/oil.nvim',
-  'NvChad/nvim-colorizer.lua',
-  'folke/which-key.nvim',
-  'nvim-pack/nvim-spectre',
-  'tpope/vim-surround',
-  'wellle/targets.vim',
+  gh 'lewis6991/gitsigns.nvim',
+  gh 'f-person/git-blame.nvim',
+  gh 'stevearc/oil.nvim',
+  gh 'NvChad/nvim-colorizer.lua',
+  gh 'folke/which-key.nvim',
+  gh 'nvim-pack/nvim-spectre',
+  gh 'tpope/vim-surround',
+  gh 'wellle/targets.vim',
 }
-
-local function add(spec)
-  local function parse(s)
-    -- If it's a shorthand (no http/git prefix and contains a slash), expand it
-    if type(s) == 'string' and not s:find '^%w+://' and s:find '/' then
-      return 'https://github.com/' .. s
-    end
-    return s
-  end
-
-  if type(spec) == 'table' then
-    for i, v in ipairs(spec) do
-      if type(v) == 'string' then
-        spec[i] = parse(v)
-      elseif type(v) == 'table' and v.src then
-        v.src = parse(v.src)
-      end
-    end
-  else
-    spec = parse(spec)
-  end
-
-  vim.pack.add(spec)
-end
-
-add(packages)
