6fcf253b40e558f6060e1e18324d136242d7e7b2
Viplov Jain <viplov.jain@gmail.com>
2026-04-25 12:55:07 +0100

    move setup to setup.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -47,7 +47,6 @@ add {
 
   'nvim-lualine/lualine.nvim',
 
-  { src = 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   'nvim-telescope/telescope.nvim',
 
   'MunifTanjim/nui.nvim',
@@ -62,28 +61,3 @@ add {
   'tpope/vim-surround',
   'wellle/targets.vim',
 }
-
-require('catppuccin').setup(require 'opts.theme')
-vim.cmd.colorscheme 'catppuccin-mocha'
-
-require 'opts.treesitter'
-require 'lsp.lspattach'
-require 'lsp.lspconfig'
-require 'lsp.lint'
-require('conform').setup(require 'opts.formatter')
-
-require 'opts.cmp'
-require 'opts.lualine'
-
-require 'opts.telescope'
-require 'opts.colorizer'
--- Highlight yanked text for 150ms
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- Setup key maps
-require('which-key').add(require('remaps').global_maps)
-require('telescope').load_extension 'fzf'
--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -1,1 +1,24 @@
+require('catppuccin').setup(require 'opts.theme')
+vim.cmd.colorscheme 'catppuccin-mocha'
 
+require 'opts.treesitter'
+require 'lsp.lspattach'
+require 'lsp.lspconfig'
+require 'lsp.lint'
+require('conform').setup(require 'opts.formatter')
+
+require 'opts.cmp'
+require 'opts.lualine'
+
+require 'opts.telescope'
+require 'opts.colorizer'
+-- Highlight yanked text for 150ms
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Setup key maps
+require('which-key').add(require('remaps').global_maps)
+require('telescope').load_extension 'fzf'
